f4ab4052474ecd8ff599e808948df9c2c469494d
Danielle Sheehan <danibsheehan@gmail.com>
2020-02-04 16:57:44 -0500

    add unsubscribe

--- src/app/components/pokemon-selector/pokemon-selector.component.ts
+++ src/app/components/pokemon-selector/pokemon-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { PokemonList } from '../../pokemon-list';
 import { PokemonListService } from '../../pokemon-list.service';
@@ -9,31 +10,43 @@ import { PokemonType } from 'src/app/pokemon-type';
   templateUrl: './pokemon-selector.component.html',
   styleUrls: ['./pokemon-selector.component.scss']
 })
-export class PokemonSelectorComponent implements OnInit {
+export class PokemonSelectorComponent implements OnInit, OnDestroy {
+
   public pokemonList: PokemonList;
-  public pokemonTypes: PokemonType[];
   public pokemonSearched: boolean;
+  public pokemonTypes: PokemonType[];
   public pokemonTypeSearched: string;
+
+  private pokemonSub: Subscription;
+  private subscriptions: Subscription;
+  private typeSub: Subscription;
 
   constructor(
     private pokemonListService: PokemonListService
   ) { }
 
   ngOnInit() {
-    this.pokemonListService.getPokemonTypes()
+    this.typeSub = this.pokemonListService.getPokemonTypes()
       .subscribe((data) => {
         this.pokemonTypes = data.results;
     });
 
-    this.pokemonListService.pokemonSearchResults
+    this.pokemonSub = this.pokemonListService.pokemonSearchResults
       .subscribe(searchResults => {
         this.pokemonList = searchResults;
       });
+
+    this.subscriptions.add(this.typeSub);
+    this.subscriptions.add(this.pokemonSub);
   }
 
   searchPokemon(name: string) {
     this.pokemonListService.getPokemon(name);
     this.pokemonTypeSearched = name;
     this.pokemonSearched = true;
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
